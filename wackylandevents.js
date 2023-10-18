b3ad645bee58394a827af6308e4b2c9da0965eeb
Ben Link <benjamin.link@centre.edu>
2023-10-18 17:25:19 -0400

    just do it

--- wackylandevents.js
+++
@@ -1,9 +0,0 @@
-function ringthebell(){
-    document.getElementById("vision").src = "los.jpg";
-    document.getElementById("storypage").innerHTML = "you have no idea what you've just done";
-    
-    document.getElementById("A View on Poetry").innerHTML = "";
-    remove("A View on Poetry");
-    document.getElementById("bell").innerHTML = "";
-    remove("bell");
-}
